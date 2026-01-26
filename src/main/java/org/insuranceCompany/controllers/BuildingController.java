ddf15234060489412e12d6a1b49f3d0304953111
Ola Darius-Ioan <darius.ola@student.unitbv.ro>
2026-01-26 20:52:26 +0200

    CRUD for Buildings

--- src/main/java/org/insuranceCompany/controllers/BuildingController.java
+++ src/main/java/org/insuranceCompany/controllers/BuildingController.java
@@ -7,21 +7,49 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/buildings")
 public class BuildingController {
-    private  final BuildingService buildingService;
-    public  BuildingController(BuildingService buildingService){
-        this.buildingService=buildingService;
+    private final BuildingService buildingService;
+
+    public BuildingController(BuildingService buildingService) {
+        this.buildingService = buildingService;
 
     }
+
     @PostMapping("/client/{clientId}")
-    public ResponseEntity<?>createBuilding(@PathVariable Long clientId, @RequestBody Building building){
-        try{
-            Building savedBuilding=buildingService.createBuilding(clientId,building);
-            return  new ResponseEntity<>(savedBuilding, HttpStatus.CREATED);
-        }catch (EntityNotFoundException e){
-            return  new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+    public ResponseEntity<?> createBuilding(@PathVariable Long clientId, @RequestBody Building building) {
+        try {
+            Building savedBuilding = buildingService.createBuilding(clientId, building);
+            return new ResponseEntity<>(savedBuilding, HttpStatus.CREATED);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
-}
+        @GetMapping("/client/{clientId}")
+        public ResponseEntity<List<Building>> getBuildingById(@PathVariable Long clientId){
+        return  new ResponseEntity<>(buildingService.getBuildingsByClient(clientId),HttpStatus.OK);
+        }
+
+        @PutMapping("/{id}")
+    public  ResponseEntity<?>updateBuilding(@PathVariable Long id ,@RequestBody Building building){
+        try{Building updated=buildingService.updateBuilding(id,building);
+            return new ResponseEntity<>(updated,HttpStatus.OK);
+        }catch (EntityNotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+
+    }
+    @DeleteMapping("/{id}")
+        public  ResponseEntity<?>deleteBuilding(@PathVariable Long id){
+        try{
+            buildingService.deleteBuilding(id);
+            return new ResponseEntity<>("Cladirea a fost stearsa",HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    }
+    }
+
--- src/main/java/org/insuranceCompany/services/BuildingService.java
+++ src/main/java/org/insuranceCompany/services/BuildingService.java
@@ -7,6 +7,8 @@ import org.insuranceCompany.repositories.ClientRepository;
 import org.insuranceCompany.users.Client;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BuildingService {
    private final ClientRepository clientRepository;
@@ -22,5 +24,21 @@ public class BuildingService {
                 .orElseThrow(()->new EntityNotFoundException("Clientul nu a fost gasit"));
         building.setOwner(owner);
         return buildingRepository.save(building);
+    }
+    public List<Building> getBuildingsByClient(Long clientId){
+        return  buildingRepository.findByOwnerId(clientId);
+    }
+    public Building updateBuilding(Long id,Building updatedBuilding) {
+                Building existingBuilding=buildingRepository.findById(id)
+                        .orElseThrow(()->new EntityNotFoundException("Cladirea nu a fost gasita"));
+                existingBuilding.setAddress(updatedBuilding.getAddress());
+                existingBuilding.setPrice(updatedBuilding.getPrice());
+                existingBuilding.setSurfaceArea(updatedBuilding.getSurfaceArea());
+                return buildingRepository.save(existingBuilding);
+    }
+    public  void deleteBuilding(Long id){
+        if(!buildingRepository.existsById(id))
+            throw new EntityNotFoundException("Cladirea nu exista");
+        buildingRepository.deleteById(id);
     }
 }
